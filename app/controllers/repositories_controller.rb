6f19856a4beba99bc1c6be10221850150332c324
Alex Zdatny <alexzdatny@gmail.com>
2019-01-17 21:52:45 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,7 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    client_id = "Iv1.e370ea355a49d6f9"
+    secret = "ea6588fe6d5f39f06435ceeb64a18c0ba19f07be"
   end
 end
