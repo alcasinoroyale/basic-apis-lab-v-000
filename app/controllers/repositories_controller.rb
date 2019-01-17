cb9d1bda37689a567c3ef570a9c89f82d66891b5
Alex Zdatny <alexzdatny@gmail.com>
2019-01-17 21:54:42 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -7,5 +7,15 @@ class RepositoriesController < ApplicationController
   def github_search
     client_id = "Iv1.e370ea355a49d6f9"
     secret = "ea6588fe6d5f39f06435ceeb64a18c0ba19f07be"
+
+    resp = Faraday.get("https://api.github.com/search/repositories") do |req|
+      req.params["q"] = params[:query]
+      req.params["client_id"] = client_id
+      req.params["client_secret"] = secret
+    end
+
+    body = JSON.parse(resp.body)
+    @results = body["items"]
+    render :search
   end
 end
